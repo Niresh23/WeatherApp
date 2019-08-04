d49ab98b9187aeb5696f6441647c6487032a0e77
Nik <niksama3@gmail.com>
2019-08-04 23:35:29 +0700

    Меню приложения

---
+++ app/src/main/java/com/nik/weather_app/MainActivity.java
@@ -0,0 +1,159 @@
+package com.nik.weather_app;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+
+import androidx.core.view.GravityCompat;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+
+import android.view.MenuItem;
+
+import com.google.android.material.navigation.NavigationView;
+
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.Menu;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Objects;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+    private MenuListAdapter adapter = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+
+        setSupportActionBar(toolbar);
+        initFloatingAction();
+        initSideMenu(toolbar);
+        initList();
+    }
+
+    private void initList() {
+        ArrayList<Integer> data = new ArrayList<>(5);
+        data.add(1);
+        data.add(2);
+        data.add(3);
+        data.add(4);
+        data.add(5);
+
+        adapter = new MenuListAdapter(data, this);
+        LinearLayoutManager manager = new LinearLayoutManager(getApplicationContext());
+
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(manager);
+        recyclerView.setAdapter(adapter);
+    }
+
+    private void initSideMenu(Toolbar toolbar) {
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    private void initFloatingAction() {
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        switch (id) {
+            case R.id.action_3_days: {
+                adapter.setThreeDays();
+                break;
+            }
+
+            case R.id.action_5_days: {
+                adapter.setFiveDays();
+                break;
+            }
+            case R.id.action_settings: {
+
+                break;
+            }
+            case R.id.action_help: {
+
+                break;
+            }
+            default: {
+                Toast.makeText(getApplicationContext(), getString(R.string.action_not_found), Toast.LENGTH_SHORT)
+                        .show();
+            }
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_home) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_tools) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
---
+++ app/src/main/java/com/nik/weather_app/MenuListAdapter.java
@@ -0,0 +1,77 @@
+package com.nik.weather_app;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class MenuListAdapter extends RecyclerView.Adapter {
+    private ArrayList<Integer> data;
+    private Activity activity;
+    private int days_count = 3;
+
+    public MenuListAdapter(ArrayList<Integer> data, Activity activity) {
+        this.activity = activity;
+        if(data != null) {
+            this.data = data;
+        } else {
+            this.data = new ArrayList<>();
+        }
+    }
+
+    void setThreeDays() {
+        int actual_days_count = days_count;
+        days_count = 3;
+        if(data.size() > 0) {
+            if(days_count != actual_days_count) {
+                for(int i = actual_days_count; i > days_count; i--)
+                    notifyItemRemoved(i - 1);
+            }
+        }
+    }
+
+    void setFiveDays() {
+        int actual_days_count = days_count;
+        days_count = 5;
+        if(data.size() > 0) {
+            if(days_count != actual_days_count) {
+                for(int i = actual_days_count; i < days_count; i++)
+                    notifyItemInserted(i );
+            }
+        }
+    }
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item_layout,
+                parent, false);
+        return  new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        String text = data.get(position) + "\u00B0" + "C";
+        ((ViewHolder)holder).textView.setText(text);
+        activity.registerForContextMenu(((ViewHolder)holder).textView);
+    }
+
+    @Override
+    public int getItemCount() {
+        return days_count;
+    }
+    class ViewHolder extends RecyclerView.ViewHolder{
+        TextView textView;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            textView = itemView.findViewById(R.id.text_list_item);
+        }
+    }
+
+}
